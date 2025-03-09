2ba98e26a42d195a275f47353dbda4beb5d4caba
Luiz Felipe Föeger <luizffsan5@gmail.com>
2025-03-09 16:12:27 -0300

    Estilização da página inicial
    Estilização da página inicial

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(session({
 }));
 
 app.set('view engine', 'ejs');
-app.set('views', 'app/views/');
+app.set('views', './app/views/');
 
 // app.set('views', path.join(__dirname, 'views'));
 // app.use(express.static(path.join(__dirname, 'public')));
