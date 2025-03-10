b2133d5dee2123e4843a9a3809cb0a762797bb1c
igorg.massi <igorg.massi@gmail.com>
2025-03-10 09:17:00 -0300

    Atualizando o controller para pegar as informações de seus agendamentos, além de ajustar e componentizar o codigo ejs.

--- app/controllers/aluno.js
+++ app/controllers/aluno.js
@@ -1,5 +1,17 @@
 module.exports.dashboard = function (app, req, res) {
-    res.render('aluno/dashboard',{flagAluno: req.session.aluno});
+    if(req.session.aluno == undefined){
+        res.redirect('/login');
+    }else{
+        const connection = app.config.dbConnection();
+        const agendamentoDAO = new app.app.models.AgendamentoDAO(connection);
+        agendamentoDAO.getAlunoById(req.session.aluno, function (error, result) {
+            agendamentoDAO.getAgendamentosByAlunoId(req.session.aluno, function (error, resultAgedamento) 
+            {
+                console.log(resultAgedamento);
+                res.render('aluno/dashboard', { aluno: result[0], agendamentos: resultAgedamento, flagAluno: req.session.aluno });
+            });
+        });
+    }
 };
 module.exports.login = function (app, req, res) {
     res.render('aluno/login', { errors: {} , flagAluno: req.session.aluno});
@@ -11,7 +23,6 @@ module.exports.authLogin = function (app, req, res) {
     req.assert('senha', 'Senha não pode ser vazia').notEmpty();
     //const errors = req.validationErrors();
 
-
     const connection = app.config.dbConnection();
     const agendamentoDAO = new app.app.models.AgendamentoDAO(connection);
     agendamentoDAO.getLogin(dadosForm, function (error, result) {
@@ -21,10 +32,14 @@ module.exports.authLogin = function (app, req, res) {
             req.session.aluno = result[0].id;
             res.redirect('/dashboard');
         } else {
-            
             //console.log('Login ou senha incorretos');
             res.render('aluno/login', { errors: [{msg: 'Nome ou senha incorretos'}], flagAluno: req.session.aluno });
             
         }
     });
 };
+module.exports.authLogout = function (app, req, res) {
+    req.session.destroy(function (error) {
+        res.redirect('/');
+    })
+}
--- app/routes/aluno.js
+++ app/routes/aluno.js
@@ -1,17 +1,14 @@
 module.exports = function(app, req, res){
     app.get('/dashboard', function(req, res){
-        if(req.session.aluno !== undefined){
-            app.app.controllers.aluno.dashboard(app, req, res);
-        }
-        else{
-            res.redirect('/login');
-        }
         app.app.controllers.aluno.dashboard(app, req, res);
     });
     app.get('/login', function(req, res){
         app.app.controllers.aluno.login(app, req, res);
     });
-    app.post('/auth', function(req, res){
+    app.post('/auth/login', function(req, res){
         app.app.controllers.aluno.authLogin(app, req, res);
+    });
+    app.get('/auth/logout', function(req, res){
+        app.app.controllers.aluno.authLogout(app, req, res);
     });
 }
