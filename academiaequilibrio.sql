098d6c54c32cf27ad4cd551ef5802055e16136fa
igorg.massi <igorg.massi@gmail.com>
2025-03-10 14:46:30 -0300

    Atualização do arquivo sql com adição de da coluna de agendamentos referente ao nome do instrutor

--- academiaequilibrio.sql
+++ academiaequilibrio.sql
@@ -16,6 +16,7 @@ CREATE TABLE agendamentos (
     atividade VARCHAR(50) NOT NULL,
     data_hora DATETIME NOT NULL,
     duracao INT NOT NULL,
+    nome_instrutor VARCHAR(50) NOT NULL,
     FOREIGN KEY (id_aluno) REFERENCES alunos(id) ON DELETE CASCADE
 );
 
@@ -26,19 +27,19 @@ INSERT INTO alunos (nome, senha) VALUES
 ('maycon', '54321');
 
 -- Inserir agendamentos para Igor
-INSERT INTO agendamento (id_aluno, atividade, data_hora, duracao) VALUES
-(1, 'Yoga', '2025-03-10 08:00:00', 60),
-(1, 'Musculação', '2025-03-12 10:30:00', 90),
-(1, 'Pilates', '2025-03-14 07:00:00', 60);
+INSERT INTO agendamento (id_aluno, atividade, data_hora, duracao, nome_instrutor) VALUES
+(1, 'Yoga', '2025-03-10 08:00:00', 60, 'João'),
+(1, 'Musculação', '2025-03-12 10:30:00', 90, 'Maria'),
+(1, 'Pilates', '2025-03-14 07:00:00', 60, 'Ana');
 
 -- Inserir agendamentos para Luis
-INSERT INTO agendamento (id_aluno, atividade, data_hora, duracao) VALUES
-(2, 'Musculação', '2025-03-09 18:00:00', 45),
-(2, 'Yoga', '2025-03-11 07:30:00', 60),
-(2, 'Musculação', '2025-03-13 09:00:00', 90);
+INSERT INTO agendamento (id_aluno, atividade, data_hora, duracao, nome_instrutor) VALUES
+(2, 'Musculação', '2025-03-09 18:00:00', 45, 'Ana'),
+(2, 'Yoga', '2025-03-11 07:30:00', 60, 'Ana'),
+(2, 'Musculação', '2025-03-13 09:00:00', 90, 'Maria');
 
 -- Inserir agendamentos para Maicon
-INSERT INTO agendamento (id_aluno, atividade, data_hora, duracao) VALUES
-(3, 'Yoga', '2025-03-08 06:00:00', 30),
-(3, 'Yoga', '2025-03-10 17:00:00', 60),
-(3, 'Yoga', '2025-03-12 08:00:00', 60);
+INSERT INTO agendamento (id_aluno, atividade, data_hora, duracao, nome_instrutor) VALUES
+(3, 'Yoga', '2025-03-08 06:00:00', 30, 'João'),
+(3, 'Yoga', '2025-03-10 17:00:00', 60, 'João'),
+(3, 'Yoga', '2025-03-12 08:00:00', 60, 'João');
