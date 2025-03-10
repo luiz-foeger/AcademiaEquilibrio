048501f94822791004d6f022734dd4bd6b8db1df
igorgmassi <igorg.massi@gmail.com>
2025-03-10 03:14:44 -0300

    Adicionando funções de bancos de dado no AgendamentoDAO além de renomaer metodos

--- app/models/AgendamentoDAO.js
+++ app/models/AgendamentoDAO.js
@@ -4,7 +4,10 @@ function AgendamentoDAO(connection) {
 AgendamentoDAO.prototype.getLogin = function (dadosForm, callback) {
     this._connection.query('SELECT * FROM alunos WHERE nome = ? AND senha = ?', [dadosForm.nome, dadosForm.senha], callback);
 }
-AgendamentoDAO.prototype.getAgendamentos = function (idAluno, callback) {
+AgendamentoDAO.prototype.getAlunoById = function (idAluno, callback) {
+    this._connection.query('SELECT * FROM alunos WHERE id = ?', idAluno, callback);
+}
+AgendamentoDAO.prototype.getAgendamentosByAlunoId = function (idAluno, callback) {
     this._connection.query('SELECT * FROM agendamentos WHERE id_aluno = ?', idAluno, callback);
 }
 module.exports = function() {
